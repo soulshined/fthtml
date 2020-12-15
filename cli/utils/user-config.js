231a5ea25a29b4144be8c7d6e2c9d59119b777ab
soulshined <davidwfreer@protonmail.com>
2020-12-14 21:02:45 -0600

    Removed comments from js output

--- cli/utils/user-config.js
+++ cli/utils/user-config.js
@@ -13,24 +13,18 @@ const config = {
 };
 if (_.fileExists(fthtmlconfig)) {
     const parsed = _.getJSONFromFile(fthtmlconfig);
-    //rootDir
     if (_.isTypeOfAndNotEmpty(parsed.rootDir, 'string'))
         config.rootDir = path.resolve(parsed.rootDir);
-    //keepTreeStructure
     if (_.isTypeOf(parsed.keepTreeStructure, 'boolean'))
         config.keepTreeStructure = parsed.keepTreeStructure;
-    //excluded
     if (Array.isArray(parsed.excluded)) {
         if (_.isTypesOfAndNotEmpty(parsed.excluded, 'string'))
             config.excluded = parsed.excluded;
     }
-    //importDir
     if (_.isTypeOfAndNotEmpty(parsed.importDir, 'string'))
         config.importDir = path.resolve(parsed.importDir);
-    //exportDir
     if (_.isTypeOfAndNotEmpty(parsed.exportDir, 'string'))
         config.exportDir = path.resolve(parsed.exportDir);
-    //templateDir
     if (_.isTypeOfAndNotEmpty(parsed.templateDir, 'string'))
         config.templateDir = path.resolve(parsed.templateDir);
 }
