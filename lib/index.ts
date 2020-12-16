f7fa3dabe45490cc4cac5dbffae10df024396bc8
soulshined <davidwfreer@protonmail.com>
2020-12-15 21:18:47 -0600

    Add deprecation notice

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
 import { ftHTMLParser } from "./parser/fthtml-parser";
 
+console.warn(`DEPRECATED: The 'template' keyword is now deprecated. Preparring for a future release; moving forward the import keyword will handle imports and template imports\n`);
+
 export function compile(src: string) {
     return new ftHTMLParser().compile(src);
 }
